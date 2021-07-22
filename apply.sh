5dcd69bf8a9e7231260263bd7c5e7cca0c74be8e
David Lantos <david.lantos@gmail.com>
2021-07-22 12:24:25 +0100

    Display current merged state at the end of apply
    Saves me making a call to the webpage every time there's an update

--- apply.sh
+++ apply.sh
@@ -2,3 +2,9 @@
 find . -name '*.cypher' \
   -exec echo "Processing " {} \; \
   -exec sh -c "docker exec -i testneo4j cypher-shell -u neo4j -p test < \$1" _ {} \;
+
+echo
+echo "𐂷 Current merged state:"
+docker exec -i testneo4j cypher-shell -u neo4j -p test <<END
+MATCH (a)-[r]->(b) RETURN labels(a), a.id, r, labels(b), b.id;
+END
